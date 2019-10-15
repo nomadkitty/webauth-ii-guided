3ba51a8ff965a7f1e0d2f8428be89f86770f6234
Jojo Zhang <jojozhang0327@gmail.com>
2019-10-15 14:58:39 -0400

    add codes

--- api/server.js
+++ api/server.js
@@ -1,21 +1,47 @@
-const express = require('express');
-const helmet = require('helmet');
-const cors = require('cors');
+const express = require("express");
+const helmet = require("helmet");
+const cors = require("cors");
+const sessions = require("express-session");
+const KnexSessionStore = require("connect-session-knex")(sessions); //for storing sessions in db
 
-const authRouter = require('../auth/auth-router.js');
-const usersRouter = require('../users/users-router.js');
+const authRouter = require("../auth/auth-router.js");
+const usersRouter = require("../users/users-router.js");
+const knexConfig = require("../database/dbConfig.js");
 
 const server = express();
+
+const sessionConfig = {
+  name: "ohfosho", //default would be 'sid'
+  secret: "keep it secret, keep it safe!", //change according to env, use an environment variable for this
+  cookie: {
+    httpOnly: true, // JS cannot access the cookies
+    maxAge: 1000 * 60 * 60, //expiration time in milliseconds
+    // secure: process.env.NODE_ENV  === production ? true : false //should be true in production
+    secure: false, //use cookie over HTTPS only. Should be true in production
+  },
+  resave: false,
+  saveUninitialized: true, // read about GDPR compliance about cookies
+
+  // change to use our database instead of memory to save the sessions
+  store: new KnexSessionStore({
+    knex: knexConfig,
+    createtable: true, //automatically create the sessions table
+    clearInterval: 1000 * 60 * 30, //delete expired sessions every 30 min
+  }),
+};
+
+//global middleware
+server.use(sessions(sessionConfig)); //turn on sessions support
 
 server.use(helmet());
 server.use(express.json());
 server.use(cors());
 
-server.use('/api/auth', authRouter);
-server.use('/api/users', usersRouter);
+server.use("/api/auth", authRouter);
+server.use("/api/users", usersRouter);
 
-server.get('/', (req, res) => {
-  res.json({ api: 'up' });
+server.get("/", (req, res) => {
+  res.json({ api: "up" });
 });
 
 module.exports = server;
--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,10 +1,10 @@
-const router = require('express').Router();
-const bcrypt = require('bcryptjs');
+const router = require("express").Router();
+const bcrypt = require("bcryptjs");
 
-const Users = require('../users/users-model.js');
+const Users = require("../users/users-model.js");
 
 // for endpoints beginning with /api/auth
-router.post('/register', (req, res) => {
+router.post("/register", (req, res) => {
   let user = req.body;
   const hash = bcrypt.hashSync(user.password, 10); // 2 ^ n
   user.password = hash;
@@ -18,23 +18,36 @@ router.post('/register', (req, res) => {
     });
 });
 
-router.post('/login', (req, res) => {
+router.post("/login", (req, res) => {
   let { username, password } = req.body;
+
+  console.log("session", req.session);
 
   Users.findBy({ username })
     .first()
     .then(user => {
       if (user && bcrypt.compareSync(password, user.password)) {
+        req.session.username = user.username;
         res.status(200).json({
           message: `Welcome ${user.username}!`,
         });
       } else {
-        res.status(401).json({ message: 'Invalid Credentials' });
+        res.status(401).json({ message: "Invalid Credentials" });
       }
     })
     .catch(error => {
       res.status(500).json(error);
     });
+});
+
+router.get("/logout", (req, res) => {
+  if (req.session) {
+    req.session.destroy(err => {
+      res.status(200).json({ message: "you can check out any time." });
+    });
+  } else {
+    res.status(200).json({ message: "already logged out" });
+  }
 });
 
 module.exports = router;
